87aa4a3c02a97539f8b88b342f569322b038c9fd
hhhan0315 <hhan0315@naver.com>
2021-05-07 21:06:41 +0900

    storyboard UI 수정 및 캐릭터명 테이블뷰 호출 처리 UserDefaults.standard 사용 탭바 컨트롤러로 UI 수정

--- LostArkCheckList/ViewController.swift
+++ LostArkCheckList/ViewController.swift
@@ -9,64 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var toDoTableView: UITableView!
-    private let toDoCellIdentifier = "toDoCell"
-    private var toDo = ["카오스 던전", "주간 레이드"]
+    @IBOutlet weak var tableView: UITableView!
+    private let userDefaults = UserDefaults.standard
+    private let cellIdentifier = "characterCell"
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        toDoTableView.delegate = self
-        toDoTableView.dataSource = self
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
+    
     @IBAction func touchUpAddButton(_ sender: UIBarButtonItem) {
-        let actionController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        
-        actionController.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
-        
-        actionController.addAction(UIAlertAction(title: "캐릭터 추가", style: .default, handler: { _ in
-            let alertController = UIAlertController(title: "캐릭터 추가", message: nil, preferredStyle: .alert)
-            alertController.addTextField(configurationHandler: nil)
-            alertController.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
-            alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: {_ in
-//                print(alertController.textFields?[0].text)
-            }))
+        let alertController = UIAlertController(title: "캐릭터 추가", message: nil, preferredStyle: .alert)
+        alertController.addTextField(configurationHandler: { (textField) in
+            textField.placeholder = "캐릭터명"
+        })
+        alertController.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { _ in
+            guard let characterName = alertController.textFields?[0].text else {
+                return
+            }
+            var characterArray = self.userDefaults.stringArray(forKey: "characterArray") ?? []
+            characterArray.append(characterName)
             
-            self.present(alertController, animated: true, completion: nil)
+            self.userDefaults.setValue(characterArray, forKey: "characterArray")
+            self.userDefaults.synchronize()
+            self.tableView.reloadData()
         }))
         
-        actionController.addAction(UIAlertAction(title: "할 일 추가", style: .default, handler: { _ in
-            let alertController = UIAlertController(title: "할 일 추가", message: nil, preferredStyle: .alert)
-            alertController.addTextField(configurationHandler: nil)
-            alertController.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
-            alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: {_ in
-//                print(alertController.textFields?[0].text)
-            }))
-            
-            self.present(alertController, animated: true, completion: nil)
-        }))
-        
-        self.present(actionController, animated: true, completion: nil)
+        self.present(alertController, animated: true, completion: nil)
     }
     
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.toDo.count
+        guard let characterArray = self.userDefaults.stringArray(forKey: "characterArray") else {
+            return 0
+        }
+        return characterArray.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: self.toDoCellIdentifier) else {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier) else {
             return UITableViewCell()
         }
-        
-        cell.textLabel?.text = toDo[indexPath.row]
+        let characterArray = self.userDefaults.stringArray(forKey: "characterArray")
+
+        cell.textLabel?.text = characterArray?[indexPath.row]
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 16)
         
         return cell
     }
-    
-    
+
+
 }
