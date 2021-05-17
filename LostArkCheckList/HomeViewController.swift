ea3c83d54c4c665a18f61ae2721bc4a74294ec22
hhhan0315 <hhan0315@naver.com>
2021-05-17 16:44:28 +0900

    UserDefaults 설정으로 복귀

--- LostArkCheckList/HomeViewController.swift
+++ LostArkCheckList/HomeViewController.swift
@@ -6,16 +6,12 @@
 //
 
 import UIKit
-import CoreData
 
 class HomeViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    private let userDefaults = UserDefaults.standard
     private let cellIdentifier = "homeCell"
-    
-    lazy var list: [NSManagedObject] = {
-        return self.fetch()
-    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,10 +36,12 @@ class HomeViewController: UIViewController {
             guard let characterName = alertController.textFields?[0].text else {
                 return
             }
+            var characterArray = self.userDefaults.stringArray(forKey: "characterArray") ?? []
+            characterArray.append(characterName)
             
-            if self.save(name: characterName) == true {
-                self.tableView.reloadData()
-            }
+            self.userDefaults.setValue(characterArray, forKey: "characterArray")
+            self.userDefaults.synchronize()
+            self.tableView.reloadData()
         })
         
         okAction.isEnabled = false
@@ -68,114 +66,73 @@ class HomeViewController: UIViewController {
         self.present(alertController, animated: true, completion: nil)
     }
     
-    func fetch() -> [NSManagedObject] {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegate.persistentContainer.viewContext
-        let fetchResult = NSFetchRequest<NSManagedObject>(entityName: "Character")
-        let result = try! context.fetch(fetchResult)
-        return result
-    }
-    
-    func save(name: String) -> Bool {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegate.persistentContainer.viewContext
-        
-        let object = NSEntityDescription.insertNewObject(forEntityName: "Character", into: context)
-        object.setValue(name, forKey: "name")
-
-        do {
-            try context.save()
-            self.list.append(object)
-            return true
-        } catch {
-            context.rollback()
-            return false
-        }
-    }
-    
-    func delete(object: NSManagedObject) -> Bool {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegate.persistentContainer.viewContext
-        
-        context.delete(object)
-        
-        do {
-            try context.save()
-            return true
-        } catch {
-            context.rollback()
-            return false
-        }
-    }
-    
 }
 
 // MARK: - UITableViewDelegate, UITableViewDataSource
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.list.count
+        guard let characterArray = self.userDefaults.stringArray(forKey: "characterArray") else {
+            return 0
+        }
+        return characterArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier) else {
             return UITableViewCell()
         }
+        guard let characterArray = self.userDefaults.stringArray(forKey: "characterArray") else {
+            return UITableViewCell()
+        }
         
-        let character = self.list[indexPath.row]
-        let name = character.value(forKey: "name") as? String
-        
-        cell.textLabel?.text = name
+        cell.textLabel?.text = characterArray[indexPath.row]
         cell.textLabel?.font = UIFont.systemFont(ofSize: 16)
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        let character = self.list[indexPath.row]
-        guard let name = character.value(forKey: "name") as? String else {
+        guard var characterArray = self.userDefaults.stringArray(forKey: "characterArray") else {
             return
         }
+        let characterName = characterArray[indexPath.row]
         
         if editingStyle == .delete {
-            let alertController = UIAlertController(title: "캐릭터 삭제", message: "\(name)\n삭제하시겠습니까?", preferredStyle: .alert)
+            let alertController = UIAlertController(title: "캐릭터 삭제", message: "\(characterName)\n삭제하시겠습니까?", preferredStyle: .alert)
             alertController.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
             alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { _ in
-                if self.delete(object: character) {
-                    self.list.remove(at: indexPath.row)
-                    self.tableView.deleteRows(at: [indexPath], with: .fade)
-                }
+                characterArray.remove(at: indexPath.row)
+                self.userDefaults.setValue(characterArray, forKey: "characterArray")
+                self.userDefaults.synchronize()
+                self.tableView.deleteRows(at: [indexPath], with: .fade)
             }))
-
+            
             self.present(alertController, animated: true, completion: nil)
         }
     }
     
-//    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-//        let
-//        self.list.swapAt(sourceIndexPath.row, destinationIndexPath.row)
-//        self.move()
-//        print(self.list)
-//        let sourceCharacter = self.list[sourceIndexPath.row]
-//        let destinationCharacter = self.list[destinationIndexPath.row
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        guard var characterArray = self.userDefaults.stringArray(forKey: "characterArray") else {
+            return
+        }
         
-        
-//
-//        characterArray.swapAt(sourceIndexPath.row, destinationIndexPath.row)
-//        self.userDefaults.setValue(characterArray, forKey: "characterArray")
-//        self.userDefaults.synchronize()
-//    }
+        characterArray.swapAt(sourceIndexPath.row, destinationIndexPath.row)
+        self.userDefaults.setValue(characterArray, forKey: "characterArray")
+        self.userDefaults.synchronize()
+    }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "characterDetailVC") else {
             return
         }
-
-        let character = self.list[indexPath.row]
-        let name = character.value(forKey: "name") as? String
         
-        nextVC.navigationItem.title = name
+        guard let characterArray = self.userDefaults.stringArray(forKey: "characterArray") else {
+            return
+        }
+        
+        nextVC.navigationItem.title = characterArray[indexPath.row]
         self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
-
+        
         self.navigationController?.pushViewController(nextVC, animated: true)
         self.tableView.deselectRow(at: indexPath, animated: true)
     }
